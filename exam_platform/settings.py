3902510b875181f13abce3f6b5ccc89615ad6757
Dev-Kazeem <shehukasimu4@gmail.com>
2026-09-13 04:21:17 +0100

    recommit reconfigured cloudinary after it failed the first time

--- exam_platform/settings.py
+++ exam_platform/settings.py
@@ -70,7 +70,6 @@ CLOUDINARY_STORAGE = {
 }
 
 
-#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
