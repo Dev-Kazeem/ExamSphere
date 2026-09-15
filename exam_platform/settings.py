e38d927e4c3a14c2a57fae6ecf064a70d4435d1e
Dev-Kazeem <shehukasimu4@gmail.com>
2026-09-16 00:47:38 +0100

    set debuge to false

--- exam_platform/settings.py
+++ exam_platform/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # a real secret key to version control.
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = os.environ.get('DEBUG' False)
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(",")
 
